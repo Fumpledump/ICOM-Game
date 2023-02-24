358daea724efdb3b8abcbcc79f029e9c41e9cb5c
Mr.Fumpledump <Fumpledump@gmail.com>
2023-02-24 11:52:31 -0500

    General Input Rework

--- ICOM_Game_Unity/Assets/Scripts/Input/InputManager.cs
+++ ICOM_Game_Unity/Assets/Scripts/Input/InputManager.cs
@@ -7,33 +7,59 @@ using UnityEngine.InputSystem.EnhancedTouch;
 [DefaultExecutionOrder(-1)]
 public class InputManager : Singleton<InputManager>
 {
+    public delegate void Swipe(Vector2 direction);
+    public event Swipe swipePerformed;
+
     private PlayerInput playerInput;
 
     private InputAction touchPositionAction;
     private InputAction touchPressAction;
 
+
+    [SerializeField] private float swipeResitance = 100;
+
+    private Vector2 startPos;
+    private Vector2 currentPos => touchPositionAction.ReadValue<Vector2>();
+
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
-        touchPositionAction = playerInput.actions["TouchPosition"];
         touchPressAction = playerInput.actions["TouchPress"];
+        touchPositionAction = playerInput.actions["TouchPosition"];
     }
 
     private void OnEnable()
     {
-        touchPressAction.performed += TouchPressed;
+        touchPressAction.performed += DetectSwipe;
+        touchPressAction.performed += _ => { startPos = currentPos; };
+        touchPressAction.canceled += _ => { startPos = currentPos; };
     }
 
     private void OnDisable()
     {
-        touchPressAction.performed -= TouchPressed;
-
+        touchPressAction.performed -= DetectSwipe;
     }
 
-    private void TouchPressed(InputAction.CallbackContext context)
+    private void DetectSwipe(InputAction.CallbackContext context)
     {
-        Vector3 position = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+        Debug.Log("Press!");
 
-        Debug.Log(position);
+        Vector2 delta = currentPos - startPos;
+        Vector2 direction = Vector2.zero;
+
+        if (Mathf.Abs(delta.x) > swipeResitance)
+        {
+            direction.x = Mathf.Clamp(delta.x, -1, 1);
+        }
+        if (Mathf.Abs(delta.y) > swipeResitance)
+        {
+            direction.y = Mathf.Clamp(delta.y, -1, 1);
+        }
+
+        if (direction != Vector2.zero & swipePerformed != null)
+        {
+            swipePerformed(direction);
+            Debug.Log(direction);
+        }
     }
 }
