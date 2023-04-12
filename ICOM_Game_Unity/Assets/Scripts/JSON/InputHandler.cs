f4face588b5cb4f54e09378986d06364da6f92ce
HestiaJ <jiangruni2020@163.com>
2023-04-12 15:11:20 -0400

    new plugin

--- ICOM_Game_Unity/Assets/Scripts/JSON/InputHandler.cs
+++ ICOM_Game_Unity/Assets/Scripts/JSON/InputHandler.cs
@@ -313,7 +313,7 @@ public class InputHandler : MonoBehaviour
 
 
     //=============================================================================
-    // Button handlers
+    // Button handlers From Native Toolkit
     //=============================================================================
 
     public void OnSaveScreenshotPress()
@@ -338,7 +338,7 @@ public class InputHandler : MonoBehaviour
 
 
     //=============================================================================
-    // Callbacks
+    // Callbacks From Native ToolKit
     //=============================================================================
 
     void ScreenshotSaved(string path)
@@ -348,7 +348,9 @@ public class InputHandler : MonoBehaviour
 
     void ImageSaved(string path)
     {
+        imagePath = path;
         console.text += "\n" + texture.name + " saved to: " + path;
+
     }
 
     void ImagePicked(Texture2D img, string path)
@@ -392,10 +394,121 @@ public class InputHandler : MonoBehaviour
     void CameraShotComplete(Texture2D img, string path)
     {
         imagePath = path;
-        //background.ImageConversion.LoadImage(path);
-        console.text += "\nCamera shot saved to: " + imagePath;
+        console.text += "\nImage picked at: " + imagePath;
+        // byte[] byteArray = File.ReadAllBytes(Application.persistentDataPath + "/" + imagePath);
+        byte[] byteArray = File.ReadAllBytes(imagePath);
+        Texture2D texture = new Texture2D(8, 8);
+        texture.LoadImage(byteArray);
+        Sprite s = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 1f);
+        collectImageHolder.sprite = s;
+
+        //string writePath = Application.persistentDataPath + $"/test_{entries.Count - 1}.png";
+        string writePath = Application.persistentDataPath + $"/{FileHandler.GenerateUniqueId()}.png";
+        curImageFilePath = writePath;
+        File.WriteAllBytes(writePath, byteArray);
+
         Destroy(img);
     }
 
+    public void OnTakePictureTest()
+    {
+        TakePicture(512);
+    }
 
+    private void TakePicture(int maxSize)
+    {
+        NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+        {
+            Debug.Log("Image path: " + path);
+            if (path != null)
+            {
+                imagePath = path;
+                // Create a Texture2D from the captured image
+                Texture2D texture = NativeCamera.LoadImageAtPath(path, maxSize);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
+
+                //// Assign texture to a temporary quad and destroy it after 5 seconds
+                //GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                //quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+                //quad.transform.forward = Camera.main.transform.forward;
+                //quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
+
+                //Material material = quad.GetComponent<Renderer>().material;
+                //if (!material.shader.isSupported) // happens when Standard shader is not included in the build
+                //    material.shader = Shader.Find("Legacy Shaders/Diffuse");
+
+                //material.mainTexture = texture;
+
+                //Destroy(quad, 5f);
+
+                //// If a procedural texture is not destroyed manually, 
+                //// it will only be freed after a scene change
+                //Destroy(texture, 5f);
+
+                Sprite s = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 1f);
+                collectImageHolder.sprite = s;
+                string writePath = Application.persistentDataPath + $"/{FileHandler.GenerateUniqueId()}.png";
+                curImageFilePath = writePath;
+                byte[] byteArray = File.ReadAllBytes(imagePath);
+                File.WriteAllBytes(writePath, byteArray);
+            }
+        }, maxSize);
+
+        Debug.Log("Permission result: " + permission);
+    }
+
+    public void OnPickTest()
+    {
+        PickImage(512);
+    }
+
+    private void PickImage(int maxSize)
+    {
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+        {
+            Debug.Log("Image path: " + path);
+            if (path != null)
+            {
+                imagePath = path;
+                // Create Texture from selected image
+                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
+
+                //// Assign texture to a temporary quad and destroy it after 5 seconds
+                //GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                //quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+                //quad.transform.forward = Camera.main.transform.forward;
+                //quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
+
+                //Material material = quad.GetComponent<Renderer>().material;
+                //if (!material.shader.isSupported) // happens when Standard shader is not included in the build
+                //    material.shader = Shader.Find("Legacy Shaders/Diffuse");
+
+                //material.mainTexture = texture;
+
+                //Destroy(quad, 5f);
+
+                //// If a procedural texture is not destroyed manually, 
+                //// it will only be freed after a scene change
+                //Destroy(texture, 5f);
+
+                Sprite s = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 1f);
+                collectImageHolder.sprite = s;
+                string writePath = Application.persistentDataPath + $"/{FileHandler.GenerateUniqueId()}.png";
+                curImageFilePath = writePath;
+                byte[] byteArray = File.ReadAllBytes(imagePath);
+                File.WriteAllBytes(writePath, byteArray);
+            }
+        });
+
+        Debug.Log("Permission result: " + permission);
+    }
 }
