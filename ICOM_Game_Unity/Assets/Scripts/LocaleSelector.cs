51a75def66e751637463c20f42f3869e0a80aa6a
HestiaJ <jiangruni2020@163.com>
2023-04-30 12:24:39 -0400

    Localization done

--- ICOM_Game_Unity/Assets/Scripts/LocaleSelector.cs
+++ ICOM_Game_Unity/Assets/Scripts/LocaleSelector.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
 
 public class LocaleSelector : MonoBehaviour
 {
     private bool active = false;
+    [SerializeField] private List<Button> lanaguageButtons;
 
     private void Start()
     {
         int ID = PlayerPrefs.GetInt("LocaleKey", 0);
         ChangeLocale(ID);
-
+        for(int i = 0; i<lanaguageButtons.Count; i++)
+        {
+            if (i != ID)
+            {
+                lanaguageButtons[i].interactable = true;
+            }
+            else
+            {
+                lanaguageButtons[i].interactable = false;
+            }
+        }
     }
     public void ChangeLocale(int localeID)
     {
-        if (isActiveAndEnabled == true)
+        if (active == true)
             return;
         StartCoroutine(SetLocale(localeID));
     }
